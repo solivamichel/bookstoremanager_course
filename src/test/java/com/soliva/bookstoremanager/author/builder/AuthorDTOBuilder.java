4d4611b87495057fc5c56fd29433cd712e733248
Michel Soliva <michels@ciandt.com>
2021-04-06 18:42:33 -0300

    Created test structure class

---
+++ src/test/java/com/soliva/bookstoremanager/author/builder/AuthorDTOBuilder.java
@@ -0,0 +1,21 @@
+package com.soliva.bookstoremanager.author.builder;
+
+import com.soliva.bookstoremanager.author.dto.AuthorDTO;
+import lombok.Builder;
+
+@Builder
+public class AuthorDTOBuilder {
+
+    @Builder.Default
+    private final Long id = 1L;
+
+    @Builder.Default
+    private final String name = "Rodrigo Peleias";
+
+    @Builder.Default
+    private final Integer age = 32;
+
+    public AuthorDTO buildAuthorDTO() {
+        return new AuthorDTO(id, name, age);
+    }
+}
