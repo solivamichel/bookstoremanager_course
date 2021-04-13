dfefa4d81a81fd605e2466208e91fb3ed9b61520
Michel Soliva <michels@ciandt.com>
2021-04-13 01:22:21 -0300

    Inserindo anotação teste

--- src/main/java/com/soliva/bookstoremanager/author/entity/AutorTeste.java
+++ src/main/java/com/soliva/bookstoremanager/author/entity/AutorTeste.java
@@ -1,9 +1,12 @@
 package com.soliva.bookstoremanager.author.entity;
 
+import lombok.Data;
+
 import javax.persistence.Entity;
 
+@Data
 @Entity
 public class AutorTeste {
 
-
+    private String id;
 }
